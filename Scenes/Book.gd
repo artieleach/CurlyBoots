ef4687f3495d12d70fc4549affeee789dc78d61a
artieleach@gmail.com <artieleach@gmail.com>
2021-08-09 15:32:39 -0500

    upd8

--- Scenes/Book.gd
+++ Scenes/Book.gd
@@ -6,16 +6,17 @@ onready var left_page : Node = get_node("Page_Left")
 onready var right_page : Node = get_node("Page_Right")
 onready var left_num : Node = get_node("Page_Number_Left")
 onready var right_num : Node = get_node("Page_Number_Right")
+onready var tween : Node = get_node("Tween")
 
 var dialogs_folder = 'res://Writing/Books'
-var book
-var cur_page = 1
+var book = []
+var cur_page = 0
 var right_page_texture
 var default_font
 var page_turn = 0
+var drawing = false
 
 func _ready():
-	initiate('book_1')
 	right_page_texture = load("res://Art/Assets/page.png")
 	default_font = DynamicFont.new()
 	default_font.font_data = load("res://Art/Fonts/m3x6.ttf")
@@ -23,6 +24,7 @@ func _ready():
 	default_font.extra_spacing_bottom = -1
 	default_font.extra_spacing_space = -1
 	process_book("book_1")
+	set_page()
 	# default_font = right_page.get_font("default_font")
 
 
@@ -47,58 +49,88 @@ func process_book(file_id):
 	var stop = 1
 	var total_width = 0
 	while start < len(file_text):
-		while total_width < 98 and stop < len(file_text):
+		while total_width < 80 and stop < len(file_text):
 			cur_line = file_text.substr(start, stop)
-			total_width = default_font.get_string_size(cur_line).x
 			stop += 1
-		while cur_line[-1] != ' ':
-			cur_line = cur_line.substr(0, len(cur_line)-1)
-			stop -= 1
+			if "\n" in cur_line:
+				break
+			total_width = default_font.get_string_size(cur_line).x
 		lines.append(cur_line)
 		start += stop - 1
 		stop = 1
 		total_width = 1
-	for i in lines:
-		print(i)
+	for i in range(0, len(lines), 10):
+		book.append(lines.slice(i, i+10))
+		
 
 func _on_TextureButton_pressed():
 	hide()
 
 
 func set_page():
-	if book.has("%s" % str(cur_page)):
-		left_page.bbcode_text = book['%s' % str(cur_page)]
+	print(book[cur_page])
+	if cur_page < len(book):
+		var out = ''
+		for i in book[cur_page]:
+			out += i + " \n"
+		left_page.bbcode_text = out
 	else:
-		left_page.bbcode_text = ''
-	left_num.text = str(cur_page)
-	right_num.text = str(cur_page + 1)
-	if book.has("%s" % str(cur_page + 1)):
-		right_page.bbcode_text = book['%s' % str(cur_page + 1)]
+		left_page.clear()
+	left_num.text = str(cur_page+1)
+	right_num.text = str(cur_page + 2)
+	if cur_page + 1 < len(book):
+		var out = ''
+		for i in book[cur_page+1]:
+			out += i + " \n"
+		right_page.bbcode_text = out
 	else:
-		right_page.bbcode_text = ''
+		right_page.clear()
 
 
 func _on_Button_Left_pressed():
-	if cur_page > 2:
+	drawing = true
+	tween.interpolate_property(self, "page_turn", -PI/2, PI/2, 1, Tween.TRANS_LINEAR)
+	tween.start()
+	left_page.clear()
+	yield(get_tree().create_timer(1), "timeout")
+	if cur_page > 1:
 		cur_page -= 2
 		set_page()
 
 
 func _on_Button_Right_pressed():
-	if cur_page < 9997:
+	drawing = true
+	tween.interpolate_property(self, "page_turn", PI/2, -PI/2, 1, Tween.TRANS_LINEAR)
+	tween.start()
+	right_page.clear()
+	yield(get_tree().create_timer(1), "timeout")
+	if cur_page < 9996:
 		cur_page += 2
 		set_page()
 
 
+func _physics_process(delta):
+	if drawing:
+		update()
+
+
 func _draw():
-	draw_set_transform_matrix(Transform2D(Vector2(1, 0), Vector2(0, 1), Vector2(0, 0)))
+	draw_set_transform_matrix(Transform2D(Vector2(sin(page_turn), 0), Vector2(0, 1), Vector2(119, 1)))
 	draw_texture(right_page_texture, Vector2(0, 0))
 	if page_turn > 0:
-		for i in range(len(right_page.text)):
-			draw_string(default_font, Vector2(1, 12*i+21), right_page.text, Color(0.1, 0.1, 0.1))
+		for line in range(len(book[cur_page + 1])):
+			draw_string(default_font, Vector2(1, 9*line+21), book[cur_page+1][line] + " ", Color(0.1, 0.1, 0.1))
 	else:
-		draw_set_transform_matrix(Transform2D(Vector2(sin(-page_turn), 0), Vector2(0, 1), Vector2(sin(page_turn)*98, 0)))
-		if book.has("%s" % str(cur_page + 2)):
-			for i in range(len(book['%s' % str(cur_page + 2)])):
-				draw_string(default_font, Vector2(1, 12*i+21), book['%s' % str(cur_page + 2)], Color(0.1, 0.1, 0.1))
+		draw_set_transform_matrix(Transform2D(Vector2(sin(-page_turn), 0), Vector2(0, 1), Vector2(sin(page_turn)*98+119, 1)))
+		if cur_page + 2 < len(book):
+			for line in range(len(book[cur_page+2])):
+				draw_string(default_font, Vector2(1, 9*line+21), book[cur_page+2][line] + " ", Color(0.1, 0.1, 0.1))
 
+
+
+func _on_HSlider_value_changed(value):
+	page_turn = value
+
+
+func _on_Tween_tween_all_completed():
+	drawing = false
