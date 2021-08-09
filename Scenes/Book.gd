430e18a00d93022f9cbd7e188c9a8eee184c9182
artieleach@gmail.com <artieleach@gmail.com>
2021-08-09 14:19:33 -0500

    upd8

--- Scenes/Book.gd
+++ Scenes/Book.gd
@@ -12,11 +12,18 @@ var book
 var cur_page = 1
 var right_page_texture
 var default_font
+var page_turn = 0
 
 func _ready():
 	initiate('book_1')
 	right_page_texture = load("res://Art/Assets/page.png")
-	default_font = Control.new().get_font("m5x8.tres")
+	default_font = DynamicFont.new()
+	default_font.font_data = load("res://Art/Fonts/m3x6.ttf")
+	default_font.extra_spacing_top = -4
+	default_font.extra_spacing_bottom = -1
+	default_font.extra_spacing_space = -1
+	process_book("book_1")
+	# default_font = right_page.get_font("default_font")
 
 
 func initiate(file_id): # Load the whole dialog into a variable
@@ -29,6 +36,30 @@ func initiate(file_id): # Load the whole dialog into a variable
 	file.close()
 	set_page()
 
+
+func process_book(file_id):
+	var file = File.new()
+	file.open("%s/%s.txt" % [dialogs_folder, file_id], file.READ)
+	var file_text = file.get_as_text()
+	var lines = []
+	var cur_line = ''
+	var start = 0
+	var stop = 1
+	var total_width = 0
+	while start < len(file_text):
+		while total_width < 98 and stop < len(file_text):
+			cur_line = file_text.substr(start, stop)
+			total_width = default_font.get_string_size(cur_line).x
+			stop += 1
+		while cur_line[-1] != ' ':
+			cur_line = cur_line.substr(0, len(cur_line)-1)
+			stop -= 1
+		lines.append(cur_line)
+		start += stop - 1
+		stop = 1
+		total_width = 1
+	for i in lines:
+		print(i)
 
 func _on_TextureButton_pressed():
 	hide()
@@ -57,3 +88,17 @@ func _on_Button_Right_pressed():
 	if cur_page < 9997:
 		cur_page += 2
 		set_page()
+
+
+func _draw():
+	draw_set_transform_matrix(Transform2D(Vector2(1, 0), Vector2(0, 1), Vector2(0, 0)))
+	draw_texture(right_page_texture, Vector2(0, 0))
+	if page_turn > 0:
+		for i in range(len(right_page.text)):
+			draw_string(default_font, Vector2(1, 12*i+21), right_page.text, Color(0.1, 0.1, 0.1))
+	else:
+		draw_set_transform_matrix(Transform2D(Vector2(sin(-page_turn), 0), Vector2(0, 1), Vector2(sin(page_turn)*98, 0)))
+		if book.has("%s" % str(cur_page + 2)):
+			for i in range(len(book['%s' % str(cur_page + 2)])):
+				draw_string(default_font, Vector2(1, 12*i+21), book['%s' % str(cur_page + 2)], Color(0.1, 0.1, 0.1))
+
--- test_throw.gd
+++ test_throw.gd
@@ -1,6 +1,6 @@
 extends Control
 
-var tex
+var tex = load("res://Art/Assets/page.png")
 var turny = -1
 var page_length = 0
 var cur_trans = Transform2D()
@@ -11,9 +11,8 @@ var test_page_2 = ["having nothing to", "do: once or twice", "she had peeped", "
 
 
 func _ready():
-	tex = load("res://Art/Assets/page.png")
 	font_holder = DynamicFont.new()
-	font_holder.font_data = load("res://Art/Fonts/m5x7.ttf")
+	font_holder.font_data = load("res://Art/Fonts/m3x6.ttf")
 	font_holder.extra_spacing_top = -4
 	font_holder.extra_spacing_bottom = -1
 	font_holder.extra_spacing_space = -1
@@ -24,11 +23,11 @@ func _draw():
 	draw_texture(tex, Vector2(0, 0))
 	if turny > 0:
 		for i in range(len(test_page)):
-			draw_string(font_holder, Vector2(1, 12*i+20), test_page[i], Color(0.1, 0.1, 0.1))
+			draw_string(font_holder, Vector2(1, 12*i+21), test_page[i], Color(0.1, 0.1, 0.1))
 	else:
 		draw_set_transform_matrix(Transform2D(Vector2(sin(-turny), 0), Vector2(0, 1), Vector2(sin(turny)*98, 0)))
 		for i in range(len(test_page_2)):
-			draw_string(font_holder, Vector2(1, 12*i+20), test_page_2[i], Color(0.1, 0.1, 0.1))
+			draw_string(font_holder, Vector2(1, 12*i+21), test_page_2[i], Color(0.1, 0.1, 0.1))
 
 
 func _physics_process(delta):
